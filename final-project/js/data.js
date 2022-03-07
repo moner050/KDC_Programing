383fc4bec8372cce2037162d9e1ab88e60eba6bf
moner050 <83222976+moner050@users.noreply.github.com>
2022-03-08 01:54:26 +0900

    Youtube페이지 코딩 이미지, css, js 수정 완료

--- final-project/js/data.js
+++ final-project/js/data.js
@@ -3,66 +3,66 @@
 var videos = [
     {
       idx: 1,
-      profile_src: 'profile.jpg',
-      thumbnail_src: 'hello.webp',
-      title: 'Adele -Hello (Official Video)',
-      author: 'adele',
-      stat: '조회수 9542만회 2일전'
+      profile_src: '1theK_profile.jpg',
+      thumbnail_src: 'strawberryMoon.webp',
+      title: '[MV] IU(아이유) - strawberry moon',
+      author: '1theK (원더케이)',
+      stat: '조회수 2555만회ㆍ4개월 전'
     },
     {
       idx: 2,
-      profile_src: 'profile.jpg',
+      profile_src: 'Adele_profile.jpg',
       thumbnail_src: 'hello.webp',
-      title: 'Adele -Hello (Official Video)',
-      author: 'adele',
-      stat: '조회수 9542만회 2일전'
+      title: 'Adele - Hello (Official Video)',
+      author: 'Adele',
+      stat: '조회수 9542만회ㆍ2일 전'
     },
     {
       idx: 3,
-      profile_src: 'profile.jpg',
-      thumbnail_src: 'hello.webp',
-      title: 'Adele -Hello (Official Video)',
-      author: 'adele',
-      stat: '조회수 9542만회 2일전'
+      profile_src: 'SIA_profile.jpg',
+      thumbnail_src: 'unstoppable.webp',
+      title: 'Sia - Unstoppable (Official Video - Live from the Nostalgic For The Present Tour)',
+      author: 'Sia',
+      stat: '조회수 2391만회ㆍ5개월 전'
     },
     {
       idx: 4,
-      profile_src: 'profile.jpg',
+      profile_src: 'Adele_profile.jpg',
       thumbnail_src: 'easy-on-me.webp',
       title: 'Adele - Easy On Me (Official Video)',
-      author: 'adele',
-      stat: '조회수 7542만회 3일전'
+      author: 'Adele',
+      stat: '조회수 7542만회ㆍ3일 전'
     },
     {
       idx: 5,
-      profile_src: 'profile.jpg',
-      thumbnail_src: 'hello.webp',
-      title: 'Adele -Hello (Official Video)',
-      author: 'adele',
-      stat: '조회수 9542만회 2일전'
+      profile_src: 'LiamPayne_profile.jpg',
+      thumbnail_src: 'StackItUp.webp',
+      title: 'Liam Payne - Stack It Up (Official Video) ft. A Boogie wit da Hoodie',
+      author: 'Liam Payne Official',
+      stat: '조회수 2036만회ㆍ2년 전'
     },
     {
       idx: 6,
-      profile_src: 'profile.jpg',
-      thumbnail_src: 'hello.webp',
-      title: 'Adele - Easy On Me (Official Video)',
-      author: 'adele',
-      stat: '조회수 9542만회 2일전'
+      profile_src: 'IU_profile.jpg',
+      thumbnail_src: 'MySea.webp',
+      title: 'My sea (아이와 나의 바다)',
+      author: '이지금 [IU Official]',
+      stat: '조회수 2244만회ㆍ1년 전'
     },
     {
       idx: 7,
-      profile_src: 'profile.jpg',
-      thumbnail_src: 'easy-on-me.webp',
-      title: 'Adele - Easy On Me (Official Video)',
-      author: 'adele',
-      stat: '조회수 7542만회 3일전'
+      profile_src: 'loote_profile.jpg',
+      thumbnail_src: 'tomorrowTonight.webp',
+      title: 'Loote - tomorrow tonight (Audio)',
+      author: 'LooteMusic',
+      stat: '조회수 1058만회ㆍ2년 전'
     },
     {
       idx: 8,
-      profile_src: 'profile.jpg',
-      thumbnail_src: 'hello.webp',
-      title: 'Adele -Hello (Official Video)',
-      author: 'adele',
-      stat: '조회수 9542만회 2일전'
+      profile_src: 'Lauv_profile.jpg',
+      thumbnail_src: 'ILikeMeBetter.webp',
+      title: 'Lauv - I Like Me Better [Official Audio]',
+      author: 'Lauv',
+      stat: '조회수 3.1억회ㆍ4년 전'
     },
   ]
--- final-project/js/main.js
+++ final-project/js/main.js
@@ -1,7 +1,7 @@
 // 메뉴 이동 
 function setMenu(_menu) {
     // setMenu 함수를 완성해주세요.
-    
+
     var menus = document.querySelectorAll(".sidebar__nav-item");
     menus.forEach(function (menu)
     {
@@ -16,6 +16,24 @@ function setMenu(_menu) {
 
 function showVideoClips() {
   // showVideoClips 함수를 완성해주세요.
+
+  var home = document.querySelector("#home .video-list");
+
+  videos.forEach(function (video)
+  {
+    var videoNode = document.querySelector(".video-item[hidden]").cloneNode(true);
+
+    videoNode.querySelector(".thumbnail").src = "./img/" + video.thumbnail_src;
+    videoNode.querySelector(".profile").src = "./img/" + video.profile_src;
+    videoNode.querySelector(".title").innerHTML = video.title;
+    videoNode.querySelector(".author").innerHTML = video.author;
+    videoNode.querySelector(".stat").innerHTML = video.stat;
+
+    videoNode.hidden = false;
+
+    home.appendChild(videoNode);
+  });
+
 }
 
 function init() {
